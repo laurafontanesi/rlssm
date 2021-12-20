544987f3c43ca24943b924c1cd4655fe393b5123
andrei <birgovanandrei>
2021-12-20 10:47:55 +0100

    dev - Initial commit - mini changes in the tests

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,2 +1,6 @@
 import rlssm
-model = rlssm.DDModel(hierarchical_levels = 2)
+
+model = rlssm.DDModel(hierarchical_levels=2)
+data = rlssm.load_example_dataset(hierarchical_levels=1)
+
+print("done")
--- tests/test_fit.py
+++ tests/test_fit.py
@@ -2,9 +2,7 @@ import rlssm
 import pandas as pd
 import os
 
-par_path = os.getcwd()
-data_path = os.path.join(par_path, 'data/data_experiment.csv')
-
+data_path = os.path.join(os.getcwd(), os.pardir, 'data', 'data_experiment.csv')
 data = pd.read_csv(data_path, index_col=0)
 
 data = data[data.participant == 20].reset_index(drop=True)
@@ -13,7 +11,7 @@ data['block_label'] += 1
 
 print(data)
 
-model = rlssm.DDModel(hierarchical_levels = 1)
+model = rlssm.DDModel(hierarchical_levels=1)
 
 # sampling parameters
 n_iter = 1000
@@ -22,10 +20,10 @@ n_thin = 1
 
 model_fit = model.fit(
     data,
-    thin = n_thin,
-    iter = n_iter,
-    chains = n_chains,
+    thin=n_thin,
+    iter=n_iter,
+    chains=n_chains,
     pointwise_waic=False,
-    verbose = False)
+    verbose=False)
 
 print(model_fit.data_info['data'])
